6305cbfed5e335c6b0d0acccdfe88f04392d8d8c
Virrodriguez1 <vir.rodriguez2004@gmail.com>
2026-05-10 11:37:21 -0300

    feat: efectos de lugares sobre barras funcionando

--- Assets/Scripts/Lugar.cs
+++ Assets/Scripts/Lugar.cs
@@ -8,13 +8,27 @@ public class Lugar : MonoBehaviour
     public float efectoDinero = 0f;
     public float efectoEstres = 0f;
 
+    private bool jugadorAdentro = false;
+
+    void Update()
+    {
+        if (jugadorAdentro)
+        {
+            barrasEstado.energia += efectoEnergia * Time.deltaTime;
+            barrasEstado.dinero += efectoDinero * Time.deltaTime;
+            barrasEstado.estres += efectoEstres * Time.deltaTime;
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
-        {
-            barrasEstado.energia += efectoEnergia;
-            barrasEstado.dinero += efectoDinero;
-            barrasEstado.estres += efectoEstres;
-        }
+            jugadorAdentro = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            jugadorAdentro = false;
     }
 }
